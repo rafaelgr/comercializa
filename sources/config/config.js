4bec2e457de5915d1ab957b4cc9f77d1523b299b
Rafael Garcia <rafa@myariadna.com>
2018-11-26 13:30:47 +0100

    Preparacion para producción

--- sources/config/config.js
+++ sources/config/config.js
@@ -1,7 +1,11 @@
 export const devConfig = {
     getApiUrl: () => {
         var urlApi = "";
-        if (!PRODUCTION) urlApi = "http://localhost:5000";
+        if (!PRODUCTION){
+            urlApi = "http://localhost:5000";
+        }else {
+            urlApi = "http://proasistencia.ddns.net:8080";
+        } 
         return urlApi;
     }
 }
